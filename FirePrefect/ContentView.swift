769dc39e71bc45e5bd99040c2b163f5b9c657062
Otabek0780 <mrdunacdtm@gmail.com>
2021-03-07 12:56:39 +0900

    Initial Commit

---
+++ FirePrefect/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirePrefect
+//
+//  Created by kim jong moon on 07/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirePrefectTests/FirePrefectTests.swift
@@ -0,0 +1,33 @@
+//
+//  FirePrefectTests.swift
+//  FirePrefectTests
+//
+//  Created by kim jong moon on 07/03/2021.
+//
+
+import XCTest
+@testable import FirePrefect
+
+class FirePrefectTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
